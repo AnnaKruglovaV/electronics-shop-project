2fd9a469bf54739c45090d58d5a2e4c413147ce4
Anna Kruglova <anya.kruglova.16@gmail.com>
2024-02-18 17:27:02 +0400

    домашка_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,13 +17,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self._name = name
+        self.__name = name
         self.price = price
-        self.quantity = quantity
-
+        self. quantity = quantity
         Item.all.append(self)
-
-
 
     def calculate_total_price(self) -> float:
         """
@@ -27,11 +28,45 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
-
+        cost_product = self.price * self.quantity
+        return cost_product
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= Item.pay_rate
+        self.price = self.price * self.pay_rate
+        """
+        Проверяет, что длина наименования товара не больше 10 симвовов. 
+        В противном случае, 
+        обрезать строку (оставить первые 10 символов)
+        """
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+            print(f'Корректное название - {value}')
+        else:
+            self.__name = value[:10]
+            print(f'Длинное слово - {value[:10]}')
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        cls.all.clear()
+        path = os.path.join(os.path.dirname(__file__), 'items.csv')
+        with open(path, 'r', newline='\n', encoding='UTF-8') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+            for item in items:
+                print(cls(name=item.get('name'),
+                          price=item.get('price'),
+                          quantity=item.get('quantity')))
+
+    @staticmethod
+    def string_to_number(name):
+        return int(float(name))
--- tests/test_item.py
+++ tests/test_item.py
@@ -12,18 +12,39 @@ def test_item_init(position):
     assert position.price == 100
     assert position.quantity == 1
 
-def test_calculate_total_price(position):
-    assert position.calculate_total_price() == 100
+def test_calculate_total_price():
+    item = Item('Смартфон', 20000, 100)
+    assert item.calculate_total_price() == 2000000
 
 
-def test_first_task_item():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-    assert item1.calculate_total_price() == 200000
-    assert item2.calculate_total_price() == 100000
+def test_apply_discount():
+    item = Item('Смартфон', 2000, 3)
+    Item.pay_rate = 0.1
 
-    Item.pay_rate = 0.8
-    item1.apply_discount()
+    item.apply_discount()
 
-    assert item1.price == 8000.0
-    assert item2.price == 20000
+    assert item.price == 200.0
+
+
+def test_name_setter():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+def test_name_setter_truncate():
+    item = Item('Телефон', 25000, 3)
+    item.name = 'Суперсмартфон'
+    assert item.name == 'Суперсмарт'
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
